0c32d0ca36f0c97c8678bf3db7285e7c2757f2fb
Dante Solorio <contact@deipi.com>
2016-07-14 21:51:13 -0500

    Initial Commit

---
+++ CallSiri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallSiri
+//
+//  Created by Dante Solorio on 7/14/16.
+//  Copyright © 2016 Dante Solorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
